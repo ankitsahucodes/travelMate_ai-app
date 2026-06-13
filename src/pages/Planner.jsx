93ee0210a84cb5017bf99ac43f05183ed3387eb9
Ankit Sahu <ankitsahu2829@gmail.com>
2026-06-14 01:22:26 +0530

    fix: route and ui

--- src/pages/Planner.jsx
+++ src/pages/Planner.jsx
@@ -30,7 +30,7 @@ function Planner() {
       setLoading(true);
 
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}trips/create`,
+        `${import.meta.env.VITE_API_URL}/trips/create`,
         formData,
         { withCredentials: true },
       );
--- src/pages/trip/TripSummary.jsx
+++ src/pages/trip/TripSummary.jsx
@@ -1,29 +1,29 @@
 function TripSummary({ trip }) {
   return (
     <div className="space-y-6">
-      <div className="bg-slate-900/80 backdrop-blur-xl border border-slate-800 rounded-2xl p-6 hover:border-sky-500 transition">
-        <div className="bg-slate-900 rounded-2xl p-5">
-          <p className="text-slate-400">Duration</p>
-          <h3 className="text-xl font-bold">{trip.duration_days} Days</h3>
-        </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+  <div className="bg-slate-900/80 backdrop-blur-xl border border-slate-800 rounded-2xl p-5">
+    <p className="text-slate-400">Duration</p>
+    <h3 className="text-xl font-bold">{trip.duration_days} Days</h3>
+  </div>
 
-        <div className="bg-slate-900 rounded-2xl p-5">
-          <p className="text-slate-400">Travelers</p>
-          <h3 className="text-xl font-bold">{trip.person_count}</h3>
-        </div>
+  <div className="bg-slate-900/80 backdrop-blur-xl border border-slate-800 rounded-2xl p-5">
+    <p className="text-slate-400">Travelers</p>
+    <h3 className="text-xl font-bold">{trip.person_count}</h3>
+  </div>
 
-        <div className="bg-slate-900 rounded-2xl p-5">
-          <p className="text-slate-400">Budget</p>
-          <h3 className="text-xl font-bold">
-            ₹{trip.budget_inr.toLocaleString()}
-          </h3>
-        </div>
+  <div className="bg-slate-900/80 backdrop-blur-xl border border-slate-800 rounded-2xl p-5">
+    <p className="text-slate-400">Budget</p>
+    <h3 className="text-xl font-bold">
+      ₹{trip.budget_inr.toLocaleString()}
+    </h3>
+  </div>
 
-        <div className="bg-slate-900 rounded-2xl p-5">
-          <p className="text-slate-400">Currency</p>
-          <h3 className="text-xl font-bold">{trip.currency}</h3>
-        </div>
-      </div>
+  <div className="bg-slate-900/80 backdrop-blur-xl border border-slate-800 rounded-2xl p-5">
+    <p className="text-slate-400">Currency</p>
+    <h3 className="text-xl font-bold">{trip.currency}</h3>
+  </div>
+</div>
 
       <div className="bg-yellow-500/10 border border-yellow-500/30 rounded-2xl p-6">
         <h3 className="font-bold text-yellow-400">Best Time To Visit</h3>
